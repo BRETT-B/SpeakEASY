dbcfb4b5e6d210c0865483d42b174272372db306
Brett D Burdick <burdickbrett@outlook.com>
2016-10-23 21:13:21 -0400

    init commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const ex = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+// Set up a environment variable for Heroku
+const port = process.env.PORT || 3000;
+// Create a app variable to configure Express application
+var app = ex();
+// Config Express static middleware
+app.use(ex.static(publicPath));
+
+
+app.listen(3000, () => {
+	console.log(`Server is listening on port ${port}`);
+});
