94f713fcc0fa5fcec676ba3ebd27cb8745734a0c
Brett D Burdick <burdickbrett@outlook.com>
2016-10-26 01:17:10 -0400

    styled the landing page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,85 +1,80 @@
-var socket = io();
-
-function scrollMessages() {
-	// selectors
-	var messages = $('#messages');
-	var newMessage = messages.children('li:last-child');
-	// heights
-	var clientHeight = messages.prop('clientHeight');
-	var scrollTop = messages.prop('scrollTop');
-	var scrollHeight = messages.prop('scrollHeight');
-	var newMessageHeight = newMessage.innerHeight();
-	var lastMessageHeight = newMessage.prev().innerHeight();
-
-	if (clientHeight + scrollTop + newMessageHeight+ lastMessageHeight >= scrollHeight) {
-		messages.scrollTop(scrollHeight);
-	}
-};
-
-socket.on('connect', function() {
-    var params = $.deparam(window.location.search);
-
-    socket.emit('join', params, function (error) {
-    	if (error) {
-    		alert(error);
-    		window.location.href = '/';
-    	} else {
-    		console.log('No error');
-    	}
+$(document).ready(function() {
+    var socket = io();
+    function scrollMessages() {
+        // selectors
+        var messages = $('#messages');
+        var newMessage = messages.children('li:last-child');
+        // heights
+        var clientHeight = messages.prop('clientHeight');
+        var scrollTop = messages.prop('scrollTop');
+        var scrollHeight = messages.prop('scrollHeight');
+        var newMessageHeight = newMessage.innerHeight();
+        var lastMessageHeight = newMessage.prev().innerHeight();
+        if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+            messages.scrollTop(scrollHeight);
+        }
+    };
+    socket.on('connect', function() {
+        var params = $.deparam(window.location.search);
+        socket.emit('join', params, function(error) {
+            if (error) {
+                alert(error);
+                window.location.href = '/';
+            } else {
+                console.log('No error');
+            }
+        });
     });
-});
-socket.on('disconnect', function() {
-    console.log('Disconnected from server')
-});
-
-socket.on('newMessage', function(message) {
-	var formattedTime = moment(message.timestamp).format('h:mm a');
-	var template = $('#message-template').html();
-	var html = Mustache.render(template, {
-		text: message.text,
-		from: message.from,
-		timestamp: formattedTime
-	});
-	$('#messages').append(html);
-	scrollMessages();
-});
-socket.on('newLocation', function (message) {
-	var formattedTime = moment(message.timestamp).format('h:mm a');
-	var template = $('#location-template').html();
-	var html = Mustache.render(template, {
-		from: message.from,
-		url: message.url,
-		timestamp: formattedTime
-	});
-	$('#messages').append(html);
-	scrollMessages();
-});
-
-$('#message-form').on('submit', function (event) {
-	event.preventDefault();
-	var messageInput = $('[name=message]');
-	socket.emit('createMessage', {
-		from: 'User',
-		text: messageInput.val()
-	}, function () {
-		messageInput.val('');
-	});
-});
-
-var locationButton = $('#send-location');
-locationButton.on('click', function () {
-	if (!navigator.geolocation) {
-		return alert('Geolocation not supported by your current browser');
-	}
-	locationButton.attr('disabled', 'disabled').text('Sending...');
-	navigator.geolocation.getCurrentPosition(function (position) {
-		locationButton.removeAttr('disabled').text('Send Location');
-		socket.emit('createLocation', {
-			latitude: position.coords.latitude,
-			longitude: position.coords.longitude
-		});
-	}, function () {
-		locationButton.removeAttr('disabled').text('Send Location');
-		alert('Unable to fetch location');
-	});
+    socket.on('disconnect', function() {
+        console.log('Disconnected from server')
+    });
+    socket.on('newMessage', function(message) {
+        var formattedTime = moment(message.timestamp).format('h:mm a');
+        var template = $('#message-template').html();
+        var html = Mustache.render(template, {
+            text: message.text,
+            from: message.from,
+            timestamp: formattedTime
+        });
+        $('#messages').append(html);
+        scrollMessages();
+    });
+    socket.on('newLocation', function(message) {
+        var formattedTime = moment(message.timestamp).format('h:mm a');
+        var template = $('#location-template').html();
+        var html = Mustache.render(template, {
+            from: message.from,
+            url: message.url,
+            timestamp: formattedTime
+        });
+        $('#messages').append(html);
+        scrollMessages();
+    });
+    $('#message-form').on('submit', function(event) {
+        event.preventDefault();
+        var messageInput = $('[name=message]');
+        socket.emit('createMessage', {
+            from: 'User',
+            text: messageInput.val()
+        }, function() {
+            messageInput.val('');
+        });
+    });
+    var locationButton = $('#send-location');
+    locationButton.on('click', function() {
+        if (!navigator.geolocation) {
+            return alert('Geolocation not supported by your current browser');
+        }
+        locationButton.attr('disabled', 'disabled').html('<i class="fa fa-refresh"></i>');
+        navigator.geolocation.getCurrentPosition(function(position) {
+            locationButton.removeAttr('disabled').html('<i class="fa fa-map-marker"></i>');
+            socket.emit('createLocation', {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            });
+        }, function() {
+            locationButton.removeAttr('disabled').html('<i class="fa fa-map-marker"></i>');
+            alert('Unable to fetch location');
+        });
+    });
 });
--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const http = require('http');
 const ex = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage, generateLocation} = require('./utils/message');
-const {validString} = require('./utils/validation');
+const { generateMessage, generateLocation } = require('./utils/message');
+const { validString } = require('./utils/validation');
 
 const publicPath = path.join(__dirname, '../public');
 // Set up a environment variable for Heroku
@@ -19,42 +19,42 @@ var io = socketIO(server);
 app.use(ex.static(publicPath));
 
 io.on('connection', (socket) => {
-	console.log('New client connection opened');
-	
+    console.log('New client connection opened');
 
-	socket.on('join', (params, callback) => {
-		if (!validString(params.name) || !validString(params.room)) {
-			callback('Display Name & Room Name are Required')
-		}
 
-		socket.join(params.room);
-		// socket.leave('')
-		//io.emit - this emits to every single connected user (method to() which sends to the given argument)
-		//socket.broadcast.emit - this sends the message to everyone except for the current user
-		//socket.emit - sends to specifically one user
-		// Emit message to welcome user from Admin
-		socket.emit('newMessage', generateMessage('Admin', 'Welcome to SpeakEASY'));
-		// Emit message to all sockets that user joined, excluding that user
-		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+    socket.on('join', (params, callback) => {
+        if (!validString(params.name) || !validString(params.room)) {
+            callback('Display Name & Room Name are Required')
+        }
 
-		callback(); 
-	});
+        socket.join(params.room);
+        // socket.leave('')
+        //io.emit - this emits to every single connected user (method to() which sends to the given argument)
+        //socket.broadcast.emit - this sends the message to everyone except for the current user
+        //socket.emit - sends to specifically one user
+        // Emit message to welcome user from Admin
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to SpeakEASY'));
+        // Emit message to all sockets that user joined, excluding that user
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
 
-	socket.on('createMessage', (message, callback) => {
-		console.log('createMessage', message);
-		io.emit('newMessage', generateMessage(message.from, message.text));
-		callback();
-	});
+        callback();
+    });
 
-	socket.on('createLocation', (coords) => {
-		io.emit('newLocation', generateLocation('Admin', coords.latitude, coords.longitude));
-	});
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback();
+    });
 
-	socket.on('disconnect', () => {
-		console.log('Client disconnected');
-	});
+    socket.on('createLocation', (coords) => {
+        io.emit('newLocation', generateLocation('Admin', coords.latitude, coords.longitude));
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
 
 server.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port ${port}`);
 });
